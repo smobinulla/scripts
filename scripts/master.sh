45eae08fb3b3420ee86a235c1f4780c42b92f824
Mobinulla <smobinulla@gmail.com>
2023-10-05 05:27:01 +0530

    Only Common.sh and Master.sh files were uploaded

--- scripts/master.sh
+++ scripts/master.sh
@@ -39,8 +39,9 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Install Claico Network Plugin Network 
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
 
-kubectl create -f custom-resources.yaml
+#kubectl create -f custom-resources.yaml
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
